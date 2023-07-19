00a4044cdf4728d46e679d26794a6bd01020c53d
KadoTomohiro <>
2023-07-19 18:11:33 +0900

    jsonの解析を、resolvedの確認のみに変更 ダンロード先を、ネームスペースに基づいたディレクトリ階層になるよう修正

--- index.js
+++ index.js
@@ -3,24 +3,28 @@ import parseJson from 'parse-json';
 import {createWriteStream} from "fs";
 import https from 'https'
 
-const nodeModulesDir = './dest'
+const nodeModulesDir = './dist'
 
 // urlの一覧を取得
 const resolvedUrls = await createResolvedUrls();
+
+console.log(`detect ${resolvedUrls.length} packages.`)
 
 // ダウンロード先のリセット
 await rm(nodeModulesDir, {force: true, recursive: true})
 await mkdir(nodeModulesDir)
 
-// ダウンロード
-// エラー処理等していないのでエラー出たらごめん
-for (let url of resolvedUrls) {
-    https.get(url, async res => {
-        const filename = url.split('/').at(-1);
-        const ws = createWriteStream(`${nodeModulesDir}/${filename}`)
-        res.pipe(ws)
-    })
+const downloadPackagesCount = await downloadPackages()
+
+
+function parseUrl(urlString) {
+    const url = new URL(urlString)
+    const urlPath = url.pathname
+    const [packagePath, fileName] = urlPath.split('/-/')
+    return {packagePath, fileName}
 }
+
+
 
 // package-lock.jsonを読み込み、,resolved urlの一覧を生成する。
 // package-lock.jsonは、<./jsons>ディレクトリにまとめること
@@ -30,7 +34,6 @@ async function createResolvedUrls() {
     const fileNames = await readdir(jsonDirPath)
 
     const resolvedList = []
-
     // ファイルごとにresolvedのURLを抽出
     for (let name of fileNames) {
         const path = `${jsonDirPath}/${name}`
@@ -43,27 +46,43 @@ async function createResolvedUrls() {
     return uniqueList
 }
 
-
 // JSONを解析して、resolvedのURLをリストアップする
 // dependenciesがネストしているので、再起的に処理する。
 function listUpResolved(json) {
     const resolvedList = [];
-    if(json.resolved) {
-        resolvedList.push(json.resolved)
-    }
-    if(json.dependencies) {
-        Object.values(json.dependencies).forEach(moduleJson => {
-            const dependencyResolvedList = listUpResolved(moduleJson)
-            resolvedList.push(...dependencyResolvedList)
-        })
-    }
-
-    // 暫定
-    if(json.packages) {
-        Object.values(json.packages).forEach(moduleJson => {
-            const dependencyResolvedList = listUpResolved(moduleJson)
-            resolvedList.push(...dependencyResolvedList)
-        })
-    }
+    Object.entries(json).forEach(([key, value]) => {
+        if (key === 'resolved') {
+            resolvedList.push(json.resolved)
+        }
+        if (typeof value === 'object') {
+            const innerResolvedList = listUpResolved(value)
+            resolvedList.push(...innerResolvedList)
+        }
+    })
     return resolvedList
+}
+
+async function downloadPackages() {
+    let counter = 0;
+
+// ダウンロード
+// エラー処理等していないのでエラー出たらごめん
+    for await (let url of resolvedUrls) {
+        https.get(url, async res => {
+            const {packagePath, fileName} = parseUrl(url);
+            const distPackagePath = `${nodeModulesDir}${packagePath}`
+            await mkdir(distPackagePath, {recursive: true}).catch((e) => {
+                if(e.code === 'EEXIST') {
+                    console.log(`skip make exist already exists dir ${distPackagePath}`)
+                } else {
+                    throw e
+                }
+            })
+
+            const ws = createWriteStream(`${distPackagePath}/${fileName}`)
+            res.pipe(ws)
+        })
+    }
+    // http.get等の仕様理解不足でカウンタが先に帰る
+    return counter;
 }
